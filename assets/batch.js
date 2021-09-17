1b48206e19ce5b4471abc95153bd6a911e2bf8a9
Deepak <deepaksinghgs30@gmail.com>
2021-09-17 09:32:38 +0530

    chart anf icons cdn added

---
+++ assets/batch.js
@@ -0,0 +1,17 @@
+// create data
+var data = [
+  {x: "A", value: 637166},
+  {x: "B", value: 721630},
+  {x: "C", value: 148662},
+  {x: "D", value: 78662},
+  {x: "E", value: 90000}
+];
+
+// create a chart and set the data
+chart = anychart.pie(data);
+
+// set the container id
+chart.container("score-student");
+
+// initiate drawing the chart
+chart.draw();
